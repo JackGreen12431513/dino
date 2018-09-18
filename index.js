0ec1fd01e705538487a71a1c43be184da70b9b22
Jacon <preazy.rblx@gmail.com>
2018-09-18 20:45:58 +0100

    Commit Title

--- index.js
+++ index.js
@@ -207,7 +207,6 @@ function writeG() {
 //git push origin master
 //git push heroku master
 
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+server.listen(config.port, config.ip, function () {
+    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+  });
