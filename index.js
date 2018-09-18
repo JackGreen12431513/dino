5f6c22846abb426936d86798ea4ab7af21f153ee
Jacon <preazy.rblx@gmail.com>
2018-09-18 20:48:30 +0100

    Commit Title

--- index.js
+++ index.js
@@ -207,6 +207,11 @@ function writeG() {
 //git push origin master
 //git push heroku master
 
-server.listen(config.port, config.ip, function () {
-    console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
-  });
+const argv = require('yargs').argv;
+const app = require('express')();
+
+const port = argv.port || 8081;
+
+app.listen(argv.port, ()=>{
+    console.log('Probably listening to heroku $PORT now ', argv.port); // unless $PORT is undefined, in which case you're listening to 8081.
+});
